5072e9d004d255b662f188bd3793e4858483c77a
StefanHMueller <55614235+StefanHMueller@users.noreply.github.com>
2024-07-15 15:02:16 +1000

    fix drift correction

--- src/tirf_tools/corrections.py
+++ src/tirf_tools/corrections.py
@@ -13,7 +13,8 @@ from dask import array as da
 import numpy as np
 from dask.diagnostics import ProgressBar
 from dask import delayed
-from scipy import signal as sig
+from scipy import fft
+import skimage
 from scipy import ndimage as ndi
 
 def div_by_gauss_da(data,sigma=20,offset=0):
@@ -53,43 +54,64 @@ def correct_data(data, sigma, darkframe):
             i['corr_data'] = corr.compute()
             
 @delayed
-def calc_shift(frameN,frame1):
-    correlation = sig.correlate(frameN,frame1, mode = 'same')
-    c_max = np.unravel_index(correlation.argmax(), correlation.shape)
+def calc_shift(frameN,frame1, filter_kernel):
+    #correlate in frequency domain
+    freq_corr =filter_kernel*fft.fft2(frameN).conj() * fft.fft2(frame1)
+    ccorr =  fft.ifftshift(
+                                fft.ifft2(freq_corr)
+                                    ).real
+    
+    
+    c_max = np.unravel_index(ccorr.argmax(), ccorr.shape)
+    # print()
     shift = np.array([[256,256]])-c_max
+    
+    # print(c_max)
     return shift
 
 @delayed
 def corr_shift(shift,frame):
-    matrix = np.array([[1,0,-shift[0]],
-                       [0,1,-shift[1]],
+    matrix = np.array([[1,0,shift[0]],
+                       [0,1,shift[1]],
                        [0,0,1]])
-    corr_frame = np.expand_dims(ndi.affine_transform(frame, matrix), axis=0)
+    corr_frame = ndi.affine_transform(frame, matrix)
     return corr_frame
 
 def drift_correct(data, key = 'corr_data'):
     if not isinstance(data,list):
         data = [data]
+    #prepare filter
+    win = skimage.filters.window('hann', (512,512))
+    win = fft.fftshift(win)
     for i in data:
         frame1 = i[key][0,0,0,:,:]   #assuming 5D image and T being the first        
         shift = []
+        
+        
+        
         for frame in range(i[key].shape[0]):
-            shift.append(calc_shift(i[key][frame,0,0,:,:],frame1))
+            shift.append(calc_shift(i[key][frame,0,0,:,:],frame1,win))
         print("Calculate drift")
         with ProgressBar():
             final = da.compute(*shift)
         final= np.concatenate(final)    #stack into np.array
-        drift_corr = i[key][0,0,0,:,:]
-        drift_corr = np.expand_dims(drift_corr,axis=0)
         
-        corr_movie = []
+        #set first frame for corrected movie
+        corr_movie = [i[key][0,0,0,:,:]]
+        
         for frame, shift_n in zip(range(1,i[key].shape[0],1),final[1:]):
-            corr_frame = corr_shift(shift_n,i[key][frame,0,0,:,:])
+            print(frame,shift_n)
+            # i[key][frame,0,0,:,:]
+            corr_frame = corr_shift(shift_n,i[key][frame,0,0,:,:])            
             corr_movie.append(corr_frame)
         print('Correct data')
         with ProgressBar():
-            movie = da.compute(*corr_movie)    
-        movie = np.concatenate(movie)   #stack into one array
+            movie = da.compute(*corr_movie)  
+        print(movie[0].shape)
+        print(movie[1].shape)
+        movie = np.stack(movie)   #stack into one array
+        while len(movie.shape)<len(data[0][key].shape):
+            movie= np.expand_dims(movie,axis=-3)
         i['drift_corr'] = movie
 
 #%%
